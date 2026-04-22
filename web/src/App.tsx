51360b1038f3eb7e5e05122157a64921bd8846f2
Eric <ericprz19@uoc.edu>
2026-04-22 13:51:49 +0200

    added web router and template translators list (#12)

---
+++ web/src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import MainLayout from './layouts/mainLayout'
+
+import Translators from "./app/translators/page.tsx"
+import Dashboard from "./app/dashboard/page.tsx"
+import Clients from "./app/clients/page.tsx"
+import Tasks from "./app/tasks/page.tsx"
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/translators" element={<Translators />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/tasks" element={<Tasks />} />
+      </Route >
+    </Routes>
+  )
+}
